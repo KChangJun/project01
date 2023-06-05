c0e8e8bb01533f65a26039ef86a9414e5bebf77e
ChangJun <projectchangjun@naver.com>
2023-06-05 22:08:12 +0900

    2023-06-05-9

--- src/main/java/com/groupc/weather/service/ManagerService.java
+++ src/main/java/com/groupc/weather/service/ManagerService.java
@@ -6,9 +6,7 @@ import org.springframework.http.ResponseEntity;
 import com.groupc.weather.common.model.AuthenticationObject;
 import com.groupc.weather.dto.ResponseDto;
 import com.groupc.weather.dto.request.manager.ActiveManagerDto;
-import com.groupc.weather.dto.request.manager.LoginManagerRequestDto;
 import com.groupc.weather.dto.request.manager.PostManagerRequestDto;
-import com.groupc.weather.dto.response.manager.LoginManagerResponseDto;
 
 public interface ManagerService {
     public ResponseEntity<ResponseDto> activeManager(AuthenticationObject authenticationObject,ActiveManagerDto dto);;
